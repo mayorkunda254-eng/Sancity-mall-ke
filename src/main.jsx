cfd190a2e00d348362614c652a87edc103daa271
mayorkunda254-eng <mayorkunda254@gmail.com>
2026-09-18 14:12:20 +0300

    Route admin path to catalogue manager

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminPage from './AdminPage.jsx'
 import './styles.css'
+
+const isAdminRoute = window.location.pathname === '/admin' || window.location.pathname.startsWith('/admin/')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isAdminRoute ? <AdminPage /> : <App />}
   </React.StrictMode>,
 )
