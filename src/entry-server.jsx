88d3855c6e81546cb4c38bfdb33717fba617b262
mayorkunda254-eng <mayorkunda254@gmail.com>
2026-09-22 01:04:03 +0300

    Support data-aware storefront prerendering

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import App from './App.jsx'
 
-export function render() {
-  return renderToString(<App />)
+export function render({ products = null, path = '/' } = {}) {
+  return renderToString(<App initialProducts={products} initialPath={path} />)
 }
